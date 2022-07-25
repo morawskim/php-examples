a48ad971097cd711f2f6331d6d7f9a15a40b5791
Marcin Morawski <marcin@morawskim.pl>
2022-07-25 18:25:56 +0200

    [AWS] SfDemo - CloudWatch SetAlarmState

--- AWS/app/src/Controller/AwsCloudWatchController.php
+++ AWS/app/src/Controller/AwsCloudWatchController.php
@@ -4,12 +4,14 @@ namespace App\Controller;
 
 use Aws\CloudWatch\CloudWatchClient;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AwsCloudWatchController
 {
-    #[Route("/cloudwatch/put-metric-data", name: 'cloudwatch', methods: ['GET'])]
+    #[Route("/cloudwatch/put-metric-data", name: 'cloudwatch.metric', methods: ['GET'])]
     public function putMetricData(CloudWatchClient $cloudWatchClient): Response
     {
         $result = $cloudWatchClient->putMetricData([
@@ -31,5 +33,23 @@ class AwsCloudWatchController
         ]);
 
         return new JsonResponse($result->toArray());
+    }
+
+    #[Route("/cloudwatch/set-alarm-state", name: 'cloudwatch.set-alarm-state', methods: ['POST'])]
+    public function setAlarmState(CloudWatchClient $cloudWatchClient, Request $request): JsonResponse
+    {
+        $alarm = $request->request->get('alarm');
+
+        if (empty($alarm)) {
+            throw new BadRequestHttpException('The alarm name is missing');
+        }
+
+        $cloudWatchClient->setAlarmState([
+            'AlarmName' => $alarm,
+            'StateValue' => 'ALARM',
+            'StateReason' => 'Testing purpose',
+        ]);
+
+        return new JsonResponse();
     }
 }
